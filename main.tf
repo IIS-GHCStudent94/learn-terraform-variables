c1075550c37ff367d9c117a37170dacc0900108a
IIS-GHCStudent94 <IIS-GHCStudent94@InnovationInSoftware.com>
2026-07-01 13:20:50 -0700

    Add load balancer security group module
    Added a security group module for the load balancer with HTTP and SSH access.

--- main.tf
+++ main.tf
@@ -146,3 +146,34 @@ module "ec2_instances" {
     environment = "development"
   }
 }
+
+module "lb_security_group" {
+  source  = "terraform-aws-modules/security-group/aws//modules/web"
+  version = "3.17.0"
+
+  name        = "lb-sg-project-alpha-dev"
+  description = "Security group for load balancer with HTTP ports open within VPC"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_with_cidr_blocks = [
+    {
+      from_port   = 22
+      to_port     = 22
+      protocol    = "tcp"
+      description = "SSH open to the world"
+      cidr_blocks = "0.0.0.0/0"
+    }
+  ]
+
+  tags = {
+    project     = "project-alpha",
+    environment = "development"
+  }
+}
+
+resource "aws_ebs_volume" "unencrypted" {
+  availability_zone = "us-west-1a"
+  size              = 8
+  encrypted         = false # Intentional violation: unencrypted EBS volume
+}
